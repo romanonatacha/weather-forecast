e09d03e2d8937c104ba509693086a72839584418
natacha romano <natacharomanonr@gmail.com>
2019-09-15 18:28:18 -0300

    carolsel

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -9,12 +9,8 @@ const API_KEY = '051f55950afd406c29fef8724b3ca520'
 export default class App extends Component {
 
   state = {
-    temperature: undefined,
-    city: undefined,
-    country: undefined,
-    humidity: undefined,
-    description: undefined,
-    error: undefined
+    data: undefined,
+    dataList: []
 }
 
 getWeather = async (e) => {
@@ -24,29 +20,37 @@ getWeather = async (e) => {
 
   let country = e.target.elements.country.value;
 
-  const api_call = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&APPID=${API_KEY}&units=metric&lang=en_us&count=10`)
+  const api_forecast = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&APPID=${API_KEY}&units=metric&lang=en_us&count=3`)
+  const api_weather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=metric&lang=en_us`)
+  
   //Convert the api data to Json format to a readable format
-  const data = await api_call.json();
+  const data = await api_forecast.json()
+  const currentData = await api_weather.json()
+  
   // If the error code is 404, set State to error message else if the city and country exists run the code 
 
   console.log(data);
+  console.log(currentData);
 
-  if (data.cod === '404') {
+
+  if (data.cod === '404' || currentData.cod === '404') {
       this.setState({
           error: 'Invalid Entry (Not Found)'
       })
   } else if (city && country) {
 
       this.setState({
-          date: data.list.dt_txt,
-          tempMin: data.list.main.temp_min,
-          tempMax: data.list.main.temp_max,
-          city: data.city.name,
-          country: data.city.country,
-          humidity: data.list.main.humidity,
-          description: data.list.weather[0].description,
-          icon: data.list.weather[0].icon,
-          error: null
+          data: data,
+          dataList: data.list,
+          currentData: currentData
+          // tempMin: data.list.main.temp_min,
+          // tempMax: data.list.main.temp_max,
+          // city: data.city.name,
+          // country: data.city.country,
+          // humidity: data.list.main.humidity,
+          // description: data.list.weather[0].description,
+          // icon: data.list.weather[0].icon,
+          // error: null
       })
   console.log(data);
 
@@ -65,15 +69,9 @@ getWeather = async (e) => {
             getWeather={this.getWeather}
           />
           <Weather
-            date={this.state.date}
-            tempMin={this.state.tempMin}
-            tempMax={this.state.tempMax}
-            humidity={this.state.humidity}
-            city={this.state.city}
-            country={this.state.country}
-            description={this.state.description}
-            icon={this.state.icon}
-            error={this.state.error}
+            data={this.state.data}
+            dataList={this.state.dataList}
+            currentData={this.state.currentData}
           />
           {/* <Capitals/> */}
         </div>
--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -1,49 +1,74 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { FormattedDate, IntlProvider, FormattedTime } from "react-intl"
 import './Weather.scss'
+import $ from 'jquery'
 
+import Carousel from "@brainhubeu/react-carousel"
+import "@brainhubeu/react-carousel/lib/style.css"
+
+$("li:empty").remove();
 export default class Weather extends Component {
-    render() {
-        return (
-            <div className="weather__info">
-          {/* If the props exist only then will the <p> tags be shown */}
 
-            { 
-              this.props.date && <p className="weather__key">Date: 
-              <span className="weather__value"> {this.props.date}</span>
-              </p>
-            }
-            { 
-              this.props.city && this.props.country && <p className="weather__key">Location: 
-              <span className="weather__value"> {this.props.city}, {this.props.country}</span>
-              </p>
-            }
-          { 
-              this.props.tempMin && this.props.tempMax && <div><p className="weather__key">Low: 
-              <span className="weather__value"> {this.props.tempMin}ºC</span>
-              </p>
-              <p className="weather__key">High: 
-              <span className="weather__value"> {this.props.tempMax}ºC</span>
-               </p></div>
+
+    
+    render() {
+
+      var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+        return (
+
+          <IntlProvider locale={navigator.language}>
+            <div className="weather">
+              { this.props.currentData &&
+                <div>
+                  <h4>{this.props.currentData.name}, {this.props.currentData.sys.country}</h4>
+                  <img className="weather__icon" src = {`http://openweathermap.org/img/w/${this.props.currentData.weather[0].icon}.png`} alt="weather_icon"/>
+                  <div>current temperature: {parseInt(this.props.currentData.main.temp)} ºC</div>
+                  <div>humidity: {this.props.currentData.main.humidity}</div>
+                  <div>{this.props.currentData.weather[0].description}</div>
+                  <h4>extended forecast</h4>
+                </div>
               }
-          { 
-              this.props.humidity && <p className="weather__key">Humidity:
-              <span className="weather__value"> {this.props.humidity} </span>
-              </p>
+              { this.props.data &&
+
+              <div id="slider">
+                <Carousel arrows infinite>
+                  {this.props.dataList.map((item, index) => {
+
+                    return (
+                      <Fragment>
+                        {item.dt_txt.substr(11, 19) === '12:00:00'
+                          ?
+                            <div className="item">
+                              <FormattedDate
+                                key={index}
+                                className="weather__info__date"
+                                value={item.dt_txt}
+                                day="numeric"
+                                month="long"
+                                year="numeric"
+                              />
+                              <span className="weather__info__temp">
+                                {parseInt(item.main.temp)} ºC
+                              </span>
+                            </div>
+                          :
+                            null
+                        }
+                      </Fragment>
+                    )
+                  })}
+                </Carousel>
+              </div>
+              
               }
-          {
-              this.props.description && this.props.icon && <p className="weather__key">Description:
-              <span className="weather__value"> {this.props.description}
-              <img className="weather__icon" src = {`http://openweathermap.org/img/w/${this.props.icon}.png`} alt="weather_icon"/>
-              </span>
-              </p>
-              }
-          {
-              this.props.error && <p className="weather__key"> 
-              <span className="weather__error"> {this.props.error}
-              </span>
-              </p>
-              }
-      </div>
+            </div>
+          </IntlProvider>
         )
     }
 }
