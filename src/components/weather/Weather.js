7a40ea543bdb4e1badc816f5ce4a2dac75c17fda
natacha romano <natacharomanonr@gmail.com>
2019-09-17 23:11:38 -0300

    almost done

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -13,53 +13,58 @@ export default class Weather extends Component {
     
     render() {
 
-      var settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
         return (
 
           <IntlProvider locale={navigator.language}>
             <div className="weather">
               { this.props.currentData &&
                 <div>
-                  <h4>{this.props.currentData.name}, {this.props.currentData.sys.country}</h4>
+                  <h4 className="weather__city">{this.props.currentData.name}, {this.props.currentData.sys.country}</h4>
                   <img className="weather__icon" src = {`http://openweathermap.org/img/w/${this.props.currentData.weather[0].icon}.png`} alt="weather_icon"/>
-                  <div>current temperature: {parseInt(this.props.currentData.main.temp)} ºC</div>
-                  <div>humidity: {this.props.currentData.main.humidity}</div>
-                  <div>{this.props.currentData.weather[0].description}</div>
-                  <h4>extended forecast</h4>
+                  <p>current temperature&ensp;<span className="bold_data">{parseInt(this.props.currentData.main.temp)} ºC</span></p>
+                  <p>humidity&ensp;<span className="bold_data">{this.props.currentData.main.humidity}</span></p>
+                  <p>{this.props.currentData.weather[0].description}</p>
                 </div>
               }
               { this.props.data &&
 
-              <div id="slider">
+
+              <div>
+                <h4 className="weather__forecast">hourly extended forecast</h4>
+                <i class="fa fa-caret-down"></i>
                 <Carousel arrows infinite>
+                  
                   {this.props.dataList.map((item, index) => {
 
                     return (
                       <Fragment>
-                        {item.dt_txt.substr(11, 19) === '12:00:00'
-                          ?
                             <div className="item">
-                              <FormattedDate
-                                key={index}
-                                className="weather__info__date"
-                                value={item.dt_txt}
-                                day="numeric"
-                                month="long"
-                                year="numeric"
-                              />
-                              <span className="weather__info__temp">
-                                {parseInt(item.main.temp)} ºC
-                              </span>
+                              <div className="weather__date">
+                                <FormattedDate
+                                  key={index}
+                                  value={item.dt_txt}
+                                  day="numeric"
+                                  month="long"
+                                  year="numeric"
+                                />
+                              </div>
+                              <div>
+                                <div className="weather__time">
+                                  <FormattedTime 
+                                    value={item.dt_txt}
+                                  />
+                                </div>
+                                <div>
+                                  <img className="weather__icon" src = {`http://openweathermap.org/img/w/${item.weather[0].icon}.png`} alt="weather_icon"/>
+                                </div>
+                                <div className="weather__temp">
+                                  {parseInt(item.main.temp)} ºC
+                                </div>
+                                <div className="weather__desc">
+                                  <div>{item.weather[0].description}</div>
+                                </div>
+                              </div>
                             </div>
-                          :
-                            null
-                        }
                       </Fragment>
                     )
                   })}
