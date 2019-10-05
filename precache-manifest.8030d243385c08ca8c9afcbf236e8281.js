5c55e0d64f0928a2d10a5f289688cc133c16fc6a
Natacha Romano <natacharomanonr@gmail.com>
2019-10-05 14:17:55 -0300

    Updates

---
+++ precache-manifest.8030d243385c08ca8c9afcbf236e8281.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8ff355d0bd35d6502b06a3ba1867d16",
+    "url": "/weather-forecast/index.html"
+  },
+  {
+    "revision": "b3a0a32410fc7f0db694",
+    "url": "/weather-forecast/static/css/2.38695677.chunk.css"
+  },
+  {
+    "revision": "140c7126b2394f3df6a5",
+    "url": "/weather-forecast/static/css/main.cefe6c22.chunk.css"
+  },
+  {
+    "revision": "b3a0a32410fc7f0db694",
+    "url": "/weather-forecast/static/js/2.e9719658.chunk.js"
+  },
+  {
+    "revision": "140c7126b2394f3df6a5",
+    "url": "/weather-forecast/static/js/main.4706675b.chunk.js"
+  },
+  {
+    "revision": "1f4587d6beb10b2bf016",
+    "url": "/weather-forecast/static/js/runtime~main.96f59320.js"
+  },
+  {
+    "revision": "240873ffd25b81318a3093f4560787ca",
+    "url": "/weather-forecast/static/media/clouds.240873ff.gif"
+  },
+  {
+    "revision": "994aa37b183cbaf9b94fd54355b166fc",
+    "url": "/weather-forecast/static/media/weather.994aa37b.gif"
+  }
+]);
